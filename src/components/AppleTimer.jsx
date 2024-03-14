9bf5dafb0400cee38f2fcf1b7a749ccb5b6fd778
Vishwa Gaurav <81325730+VishwaGauravIn@users.noreply.github.com>
2024-03-15 00:33:37 +0530

    impv

--- src/components/AppleTimer.jsx
+++ src/components/AppleTimer.jsx
@@ -7,8 +7,10 @@ import { motion, AnimatePresence } from "framer-motion";
 export default function AppleTimer() {
   const [time, setTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
+    setIsMounted(true);
     let interval = null;
 
     if (isRunning) {
@@ -43,51 +45,71 @@ export default function AppleTimer() {
     setIsRunning(false);
   };
   return (
-    <div className="w-96 h-20 rounded-full p-4 flex justify-between items-center bg-black shadow-md">
-      <div className="flex gap-2 h-full">
-        <button
-          className="h-full group aspect-square bg-yellow-600/40 hover:bg-yellow-600/50 rounded-full flex justify-center items-center transition-all ease-in-out duration-300"
-          onClick={() => {
-            isRunning ? handlePause() : handleStart();
-          }}
+    <>
+      {isMounted && (
+        <motion.div
+          className="w-96 h-20 rounded-full p-4 flex justify-between items-center bg-black shadow-md"
+          initial={{ scale: 0, opacity: 0 }}
+          animate={{ scale: [1, 1.05, 1], opacity: 1 }}
+          transition={{ duration: 0.5, ease: "easeInOut" }}
         >
-          <div className="group-active:scale-90 transition-all ease-in-out duration-300">
-            {isRunning ? (
-              <FaPause className="text-yellow-400 h-6" />
-            ) : (
-              <FaPlay className="text-yellow-400 h-6 -mr-0.5" />
-            )}
+          <div className="flex gap-2 h-full">
+            <button
+              className="h-full group aspect-square bg-yellow-600/40 hover:bg-yellow-600/50 rounded-full flex justify-center items-center transition-all ease-in-out duration-300"
+              onClick={() => {
+                isRunning ? handlePause() : handleStart();
+              }}
+            >
+              <div className="group-active:scale-90 transition-all ease-in-out duration-300">
+                {isRunning ? (
+                  <FaPause className="text-yellow-400 h-6" />
+                ) : (
+                  <FaPlay className="text-yellow-400 h-6 -mr-0.5" />
+                )}
+              </div>
+            </button>
+            <button
+              className="h-full aspect-square bg-zinc-500/40 hover:bg-zinc-500/50 rounded-full flex justify-center items-center group transition-all ease-in-out duration-300"
+              onClick={handleReset}
+            >
+              <FaX className="text-zinc-100 h-6 group-active:scale-90 transition-all ease-in-out duration-300" />
+            </button>
           </div>
-        </button>
-        <button
-          className="h-full aspect-square bg-zinc-500/40 hover:bg-zinc-500/50 rounded-full flex justify-center items-center group transition-all ease-in-out duration-300"
-          onClick={handleReset}
-        >
-          <FaX className="text-zinc-100 h-6 group-active:scale-90 transition-all ease-in-out duration-300" />
-        </button>
-      </div>
-      <div className="flex items-end gap-2 text-yellow-500 w-44 h-full">
-        <span className="text-lg font-medium mb-1">Timer</span>
-        <p className="text-[44px] leading-[1] font-thin flex overflow-hidden w-[110px] h-full justify-center">
-          {formatTime(time).map((digit, index) => (
-            <AnimatePresence mode="wait">
-              <motion.p
-                key={`${digit}-${index}`}
-                initial={{ y: 5, opacity: 0, color: "rgb(254 252 232)" }}
-                animate={{ y: 0, opacity: 1, color: "rgb(234 179 8)" }}
-                exit={{ y: -5, opacity: 0, color: "rgb(254 252 232)" }}
-                transition={{
-                  duration: 0.25,
-                  ease: "easeInOut",
-                  exit: { duration: 1, ease: "easeInOut" },
-                }}
-              >
-                {digit}
-              </motion.p>
-            </AnimatePresence>
-          ))}
-        </p>
-      </div>
-    </div>
+          <div className="flex items-end gap-2 text-yellow-500 w-44 h-full">
+            <span className="text-lg font-medium mb-1">Timer</span>
+            <p
+              className="text-[44px] leading-[1] font-thin flex overflow-hidden w-[110px] h-full justify-center"
+              suppressHydrationWarning
+            >
+              {formatTime(time).map((digit, index) => (
+                <AnimatePresence mode="wait">
+                  <motion.p
+                    key={`${digit}-${index}`}
+                    initial={{
+                      y: [10, 5],
+                      opacity: 0,
+                      color: "rgb(254 252 232)",
+                    }}
+                    animate={{ y: 0, opacity: 1, color: "rgb(234 179 8)" }}
+                    exit={{
+                      y: [-5, -50],
+                      opacity: 0,
+                      color: "rgb(254 252 232)",
+                    }}
+                    transition={{
+                      duration: 0.3,
+                      ease: "easeInOut",
+                      exit: { duration: 1, ease: "easeInOut" },
+                    }}
+                  >
+                    {digit}
+                  </motion.p>
+                </AnimatePresence>
+              ))}
+            </p>
+          </div>
+        </motion.div>
+      )}
+    </>
   );
 }
